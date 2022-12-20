d010b9fe4c3ec381bd3b4298351204b26d338a3b
Goodluck Okon <goodluckokon0@gmail.com>
2022-12-20 15:23:36 +0100

    done

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+M0dule 0-square
+Define class Square
+"""
+
+
+class Square:
+    """Defines a square.
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+M0dule 1-square
+Define class Square
+"""
+
+
+class Square:
+    """Defines a Class square object.
+    Private instance attribute: size.
+    """
+
+    def __init__(self, size):
+        """Initialize method that stores the size of the square.
+        """
+        self.__size = size
